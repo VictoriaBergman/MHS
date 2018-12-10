fccb90550d66c868e8fe964cfb77af8fe000d711
myhrnz1 <myhrnz@outlook.com>
2018-12-10 16:28:37 +0100

    add method indexOfAccount/ALL

--- MHS_Bank.java
+++ MHS_Bank.java
@@ -2,7 +2,15 @@ import java.util.*;
 public class MHS_Bank{
 
 public ArrayList<MHS_BankAccount> accountList = new ArrayList<MHS_BankAccount>();
-
+  public int indexOfAccount(String accountNumber) {
+    int index;
+    for( index = 0; index < accountList.size(); index++ ) {
+      if (accountList.get(index).accountNumber.equals(accountNumber)) {
+        break;
+      }
+    }
+    return index;
+  }
 
 
 
