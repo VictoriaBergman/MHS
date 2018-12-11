066f28c48079ef2494de8f11653a1c1c0e172a5e
myhrnz1 <myhrnz@outlook.com>
2018-12-11 09:49:17 +0100

    create empty methods checkPayment, reset/ALL

--- MHS_Main.java
+++ MHS_Main.java
@@ -1,7 +1,16 @@
 public class MHS_Main{
 
   public MHS_Main(){
-    
+    // create banks
+    // add accounts to each bank
+  }
+
+  public Payment checkPayment(String accountNumber, double totalSum) {
+    // method that communicates with TIS
+  }
+
+  public reset(){
+    // reset Payment ?? Or make sure this happens in checkPayment??
   }
 
 }
