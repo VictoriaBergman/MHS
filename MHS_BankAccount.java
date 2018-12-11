e336524729a6fae9d586494db6cbf2e7f9e7ec8b
myhrnz1 <myhrnz@outlook.com>
2018-12-11 09:43:12 +0100

    create 2-param constructor/All

--- MHS_BankAccount.java
+++ MHS_BankAccount.java
@@ -2,10 +2,8 @@ public class MHS_BankAccount{
   String accountNumber;
   double balance;
 
-public MHS_BankAccount (){
-
-accountNumber = "";
-balance = 0;
-}
-
+public MHS_BankAccount (String inputAccountNumber, double inputBalance){
+  accountNumber = inputAccountNumber;
+  balance = inputBalance;
+  }
 }
