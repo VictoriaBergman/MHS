00990bed260516b99672384d7961c1b3c8d19d48
JonasPersson78 <jonasnpersson@gmail.com>
2018-12-11 10:46:23 +0100

    Check spelling/ALL

--- MHS_Main.java
+++ MHS_Main.java
@@ -1,8 +1,9 @@
 public class MHS_Main{
 
   public MHS_Main(){
+    //Creat banks.
     MHS_Bank nordea = new MHS_Bank();
-    MHS_Bank handlesbanken = new MHS_Bank();
+    MHS_Bank handelsbanken = new MHS_Bank();
     MHS_Bank seb = new MHS_Bank();
     MHS_Bank swedbank = new MHS_Bank();
     // add accounts to each bank
